239eb4e05577e99274fb8b909aa32d1efcdf8fb7
louisbouchard <louis.bouchard@cmontmorency.qc.ca>
2024-01-16 22:14:36 -0500

    CodeBehind ContactForm

---
+++ CafeUrbania.UI/Components/ContactForm.razor.cs
@@ -0,0 +1,16 @@
+﻿using CafeUrbania.Models;
+
+namespace CafeUrbania.UI.Components;
+
+public partial class ContactForm
+{
+    public Contact Contact { get; set; } = new Contact();
+
+    public bool HasContacted = false;
+
+    private async void HandleValidSubmit()
+    {
+        HasContacted = true;
+        Contact = new();
+    }
+}
